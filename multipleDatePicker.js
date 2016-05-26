be6efc4dcd4bba67747348ccad0bdff335624b72
Andreas Zaschka <andreas.zaschka@mz-solutions.de>
2016-05-26 11:44:37 +0200

    removed month-navigate

--- multipleDatePicker.js
+++ multipleDatePicker.js
@@ -82,11 +82,6 @@
                 disallowGoFuturMonths: '=?',
                 /*
                  * Type: boolean
-                 * if true buttons for switching months aren`t shown
-                 * */
-                showBackFutureMonths: '=?',
-                /*
-                 * Type: boolean
                  * if true empty boxes will be filled with days of previous/next month
                  * */
                 showDaysOfSurroundingMonths: '=?',
@@ -113,9 +108,7 @@
             },
             template: '<div class="multiple-date-picker">' +
             '<div class="picker-top-row">' +
-            '<div class="text-center picker-navigate picker-navigate-left-arrow" ng-class="{\'disabled\':disableBackButton}" ng-click="previousMonth()" ng-show="showBackFutureMonths">&lt;</div>' +
             '<div class="text-center picker-month">{{month.format(\'MMMM YYYY\')}}</div>' +
-            '<div class="text-center picker-navigate picker-navigate-right-arrow" ng-class="{\'disabled\':disableNextButton}" ng-click="nextMonth()" ng-show="showBackFutureMonths">&gt;</div>' +
             '</div>' +
             '<div class="picker-days-week-row">' +
             '<div class="text-center" ng-repeat="day in daysOfWeek">{{day}}</div>' +
